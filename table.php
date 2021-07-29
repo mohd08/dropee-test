716d08a00964c34fc63c4b6c871c0c1c88150af7
Mohd Arif <47680350+mohd08@users.noreply.github.com>
2021-07-29 12:48:13 +0800

    Add files via upload

---
+++ table.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css" type="text/css">
+    </head>
+    
+    <body>
+        <table >
+            <?php
+            echo "<tr><th> </th> <th> Dropee.com </th> <th> </th> <th> Built Trust </th> </tr>";
+            echo "<tr> <td>  </td> <td>  </td> <td> SaaS enabled marketplace </td> <td>  </td> </tr>";
+            echo "<tr> <td> B2B Marketplace </td> <td>  </td> <td>  </td> <td>  </td> </tr>";
+            echo "<tr> <td> </td> <td>  </td> <td>  </td> <td> Provide Transparency </td> </tr>";
+            ?>
+        </table>
+    </body>
+</html>
